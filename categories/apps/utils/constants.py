905cdcacf08fb7daf239cf13e5aa77446083acb2
Adrian Carrascal <acarrascalgarcia@gmail.com>
2021-01-24 21:38:09 -0500

    Remove categories

--- categories/apps/utils/constants.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-AWS_REGION = 'us-east-1'
-BASE_URL = getenv('BASE_URL')
-CSV_DELIMITER = ';'
-DYNAMODB_HOST = 'https://dynamodb.eu-central-1.amazonaws.com'
-DYNAMODB_TABLE = getenv('DYNAMODB_TABLE')
-SETUP_BUCKET = getenv('SETUP_BUCKET')
